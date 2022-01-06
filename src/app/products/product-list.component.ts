d3bf74d91aea884cd7a21c9207f9726187fe63a0
ShaniGeva-Next <shani.g@next-insurance.com>
2022-01-06 13:43:44 +0200

    async pipe

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,35 +1,27 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
   categories : any;
 
-  products: Product[] = [];
-  sub: Subscription;
+  products$: Observable<Product[]>;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe({
-        next: (products) => this.products = products,
-        error: (error) => this.errorMessage = error
-      });
+    this.products$ = this.productService.getProducts();
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,12 +1,12 @@
 /* Defines the product entity */
 export interface Product {
-  id: number;
-  productName: string;
-  productCode?: string;
-  description?: string;
-  price?: number;
-  categoryId?: number;
-  quantityInStock?: number;
-  searchKey?: string[];
-  supplierIds?: number[];
+  id: number,
+  productName: string,
+  productCode?: string,
+  description?: string,
+  price?: number,
+  categoryId?: number,
+  quantityInStock?: number,
+  searchKey?: string[],
+  supplierIds?: number[],
 }
